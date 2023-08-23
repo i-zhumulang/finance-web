15f732f4b74467a28f451acdfe13ee7995d9c677
i-zhumulang <1065953740@qq.com>
2023-08-23 11:43:07 +0800

    2023-08-23 11:43
    1、代码优化

--- src/typescripts/Common/Container/ContainerClass.ts
+++ src/typescripts/Common/Container/ContainerClass.ts
@@ -1,14 +1,12 @@
 import {reactive} from "vue";
 import BaseClass from "@/typescripts/Common/Common/Objects/BaseClass";
-import type {DataType} from "@/typescripts/Common/Container/ContainerClassInterface";
+import type {ContainerDataInterface} from "@/typescripts/Common/Container/ContainerClassInterface";
 
 export default class ContainerClass extends BaseClass {
-    public data = reactive<DataType>({
+    public data = reactive<ContainerDataInterface>({
         user: {
             name: ''
         },
-        module: "",
-        moduleId: "",
         aside: [],
         asideActivePath: ""
     });
@@ -58,53 +56,53 @@ export default class ContainerClass extends BaseClass {
     public initAside(): void {
         this.data.aside = [
             {
-                id: '1',
+                id: 10,
                 name_zh: "首页",
                 children: [
                     {
-                        id: '1-1',
+                        id: 101,
                         component: '/index',
                         name_zh: "控制台",
                     }
                 ]
             },
             {
-                id: '2',
+                id: 20,
                 name_zh: "基础设置",
                 children: [
                     {
-                        id: '2-1',
+                        id: 201,
                         component: '/family',
                         name_zh: "家庭组",
                     },
                     {
-                        id: '2-2',
+                        id: 202,
                         component: '/family-invite',
                         name_zh: "家庭邀请",
                     },
                     {
-                        id: '2-3',
+                        id: 203,
                         component: '/category',
                         name_zh: "支出分类",
                     },
                     {
-                        id: '2-4',
+                        id: 204,
                         component: '/payment-method',
                         name_zh: "支付方式",
                     },
                     {
-                        id: '2-5',
+                        id: 205,
                         component: '/payment-account',
                         name_zh: "支付账号",
                     },
                 ]
             },
             {
-                id: '3',
+                id: 30,
                 name_zh: "财务信息",
                 children: [
                     {
-                        id: '3-1',
+                        id: 301,
                         component: '/financial-statement',
                         name_zh: "财务流水",
                     },
